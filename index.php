c71a29091d52064890d051d28dc8e0e8ce55b8af
PaulSIO <paulastru@gmail.com>
2019-09-11 10:50:26 +0200

    nombre d'occurences dans un tableau

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
 <body>
 <?php
 
-<<<<<<< HEAD
 $nb=11;
 $compteur=0;
 $x=rand(0,1000);
@@ -19,9 +18,7 @@ while($x!=$nb)
   $compteur++;
 }
 echo $nb ."trouvé en"." ".$compteur." "."tirages";
-=======
 
->>>>>>> dd7d721c4efccbf19032644a489b7d88496b2d9d
 ?>
 </body>
 </html>

1c79657ca1e8d9fa41c92c29d80c6e42c90c92b2
PaulSIO <paulastru@gmail.com>
2019-09-11 09:49:52 +0200

    ajout d'un php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$nb=11;
+$compteur=0;
+$x=rand(0,1000);
+while($x!=$nb)
+{
+  $x=rand(0,1000);
+  $compteur++;
+}
+echo $nb ."trouvé en"." ".$compteur." "."tirages";
+?>
+</body>
+</html>
